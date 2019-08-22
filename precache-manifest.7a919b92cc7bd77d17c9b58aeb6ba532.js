92e31718d1339f5d68db8ae1cbf056671a4eb944
Tom <tom@adoplayer.com>
2019-08-22 14:59:17 +0300

    Updates

---
+++ precache-manifest.7a919b92cc7bd77d17c9b58aeb6ba532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9e14f3f6a5f5cbbdf2b0d4e0163080",
+    "url": "/qtracker/index.html"
+  },
+  {
+    "revision": "f04ecb7fff0aabb1d3de",
+    "url": "/qtracker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bdeeb69a0ed9f1b020a9",
+    "url": "/qtracker/static/js/2.512e4ff0.chunk.js"
+  },
+  {
+    "revision": "f04ecb7fff0aabb1d3de",
+    "url": "/qtracker/static/js/main.738d7b7c.chunk.js"
+  },
+  {
+    "revision": "51a9e09b33ad301e10fb",
+    "url": "/qtracker/static/js/runtime~main.a5585ae3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qtracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
